e22aff7f441ad8f021f186989f00563672404d46
Pablo Salani <salanipablo@gmail.com>
2024-01-06 11:57:48 -0300

    First commit

---
+++ src/presentation/server.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import path from "path";
+
+interface Options {
+  port: number;
+  publicPath?: string;
+}
+
+export class Server {
+  private app = express();
+  private port: number;
+  private publicPath: string;
+
+  constructor(options: Options) {
+    const {port, publicPath = "public"} = options;
+    this.port = port;
+    this.publicPath = publicPath;
+  }
+
+  async start() {
+    //*Middlewares
+
+    //*Public Folder
+
+    this.app.use(express.static(this.publicPath));
+
+    this.app.get("*", (req, res) => {
+      const indexPath = path.join(
+        __dirname + `../../../${this.publicPath}/index.html`
+      );
+      res.sendFile(indexPath);
+    });
+
+    this.app.listen(this.port, () => {
+      console.log("Server Running PORT:3000");
+    });
+  }
+}
